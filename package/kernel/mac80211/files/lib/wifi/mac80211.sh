ec653f2bb82b070ac54b69b892fdafe701481e60
boos4721 <3.1415926535boos@gmail.com>
2021-07-19 19:18:17 +0800

    mac80211: distinguish ssid
    Signed-off-by: boos4721 <3.1415926535boos@gmail.com>

--- package/kernel/mac80211/files/lib/wifi/mac80211.sh
+++ package/kernel/mac80211/files/lib/wifi/mac80211.sh
@@ -181,7 +181,8 @@ detect_mac80211() {
 			set wireless.default_radio${devidx}.device=radio${devidx}
 			set wireless.default_radio${devidx}.network=lan
 			set wireless.default_radio${devidx}.mode=ap
-			set wireless.default_radio${devidx}.ssid=OpenWrt
+            		set wireless.default_radio0.ssid=OpenWrt_2.4G
+            		set wireless.default_radio1.ssid=OpenWrt_5G
 			set wireless.default_radio${devidx}.encryption=none
 EOF
 		uci -q commit wireless
