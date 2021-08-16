e970163a25e651c76ab06decc75d98c78fc7af04
boos4721 <3.1415926535boos@gmail.com>
2021-08-17 00:40:44 +0800

    fstools: update to git HEAD  629726d blockd: fix resource leak discovered by coverity scan  68ae639 libubi: fix several issues discovered by Coverity  a77c4fa ubi: fix resource leak in legacy codepath  2e3aca2 block: fix two resources leaks discovered by Coverity
    Signed-off-by: boos4721 <3.1415926535boos@gmail.com>

--- .build.sh
+++ .build.sh
@@ -32,7 +32,7 @@ Old_Version="$(egrep -o "R[0-9]+\.[0-9]+\.[0-9]+" ${Version_File})"
 sed -i "s?By?By $id ?g" package/base-files/files/etc/banner
 sed -i "s?Openwrt?Openwrt ${Openwrt_Version}?g" package/base-files/files/etc/banner
 sed -i '/profile/d' package/base-files/files/lib/upgrade/keep.d/base-files-essential
-sed -i "s?${Old_Version}?${Old_Version} Compiled by $id ?g" ${Version_File}
+sed -i "s?${Old_Version}?${Old_Version} By $id ?g" ${Version_File}
 }
 
 
