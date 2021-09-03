aa170049b4ca38e07f0fa76e69e40a794b4102c9
boos4721 <3.1415926535boos@gmail.com>
2021-09-03 20:02:39 +0800

    kernel: bump 5.10 to 5.10.62
    Signed-off-by: boos4721 <3.1415926535boos@gmail.com>

--- .build.sh
+++ .build.sh
@@ -6,7 +6,7 @@
 
 export ARCH=amd64
 export SUBARCH=amd64
-export HOME=/drone
+#export HOME=/drone
 export FORCE_UNSAFE_CONFIGURE=1
 export FORCE=1
 ln -sf /usr/share/zoneinfo/Asia/Shanghai /etc/localtime
@@ -16,12 +16,12 @@ SETUP() {
 apt-get update 
 apt-get -y install build-essential asciidoc binutils bzip2 gawk gettext git libncurses5-dev libz-dev patch python3 python2.7 unzip zlib1g-dev lib32gcc1 libc6-dev-i386 subversion flex uglifyjs git-core gcc-multilib p7zip p7zip-full msmtp libssl-dev texinfo libglib2.0-dev xmlto qemu-utils upx libelf-dev autoconf automake libtool autopoint device-tree-compiler g++-multilib antlr3 gperf wget curl swig rsync    
 git config --global user.email 3.1415926535boos@gmail.com
-git config --global user.name $id
+git config --global user.name Boos4721
 }
 
 CLONE() {
-git clone https://github.com/$id/OpenWrt-Packages package/Boos --depth=1 
-wget -O .config https://gitlab.com/$id//openwrt/-/raw/master/.config && wget -O package/base-files/files/etc/profile https://gitlab.com/$id/openwrt/-/raw/master/profile && wget -O package/base-files/files/etc/banner https://gitlab.com/$id/openwrt/-/raw/master/banner
+git clone https://github.com/Boos4721/OpenWrt-Packages package/Boos --depth=1
+wget -O .config https://gitlab.com/Boos4721//openwrt/-/raw/master/.config && wget -O package/base-files/files/etc/profile https://gitlab.com/Boos4721/openwrt/-/raw/master/profile && wget -O package/base-files/files/etc/banner https://gitlab.com/Boos4721/openwrt/-/raw/master/banner
 ./scripts/feeds update -a -f
 ./scripts/feeds install -a -f
 }
@@ -29,10 +29,10 @@ wget -O .config https://gitlab.com/$id//openwrt/-/raw/master/.config && wget -O 
 MAIN() {
 Version_File="package/lean/default-settings/files/zzz-default-settings"
 Old_Version="$(egrep -o "R[0-9]+\.[0-9]+\.[0-9]+" ${Version_File})"
-sed -i "s?By?By $id ?g" package/base-files/files/etc/banner
+sed -i "s?By?By Boos4721 ?g" package/base-files/files/etc/banner
 sed -i "s?Openwrt?Openwrt ${Openwrt_Version}?g" package/base-files/files/etc/banner
 sed -i '/profile/d' package/base-files/files/lib/upgrade/keep.d/base-files-essential
-sed -i "s?${Old_Version}?${Old_Version} By $id ?g" ${Version_File}
+sed -i "s?${Old_Version}?${Old_Version} By Boos4721 ?g" ${Version_File}
 }
 
 
@@ -55,7 +55,7 @@ echo "cat cowtransfer.log | grep https"
 GITHUB_UPLOAD() {
 cd ~/UPLOAD
 git init
-git remote add origin https://$token@github.com/$id/updater.git
+git remote add origin https://$token@github.com/Boos4721/updater.git
 git checkout -b OpenWrt
 git add .
 git commit -sm "$(date +"%m%d-%H%S")"
