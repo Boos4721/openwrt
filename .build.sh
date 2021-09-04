d196d6ba7ad99038c7d635c400641c2346f5fe16
boos4721 <3.1415926535boos@gmail.com>
2021-09-04 19:18:14 +0800

    CI: Optimization script
    Signed-off-by: boos4721 <3.1415926535boos@gmail.com>

--- .build.sh
+++ .build.sh
@@ -1,20 +1,17 @@
 #!/usr/bin/env bash
 
 # OpenWrt CI Builder
-# Copyright (C) 2021 @Boos4721(Telegram and Github)  
+# Copyright (C) 2021 @Boos4721(Telegram and Github)
 # Default Settings
 
 export ARCH=amd64
 export SUBARCH=amd64
-#export HOME=/drone
 export FORCE_UNSAFE_CONFIGURE=1
 export FORCE=1
-ln -sf /usr/share/zoneinfo/Asia/Shanghai /etc/localtime
+sudo ln -sf /usr/share/zoneinfo/Asia/Shanghai /etc/localtime
 
 
 SETUP() {
-apt-get update 
-apt-get -y install build-essential asciidoc binutils bzip2 gawk gettext git libncurses5-dev libz-dev patch python3 python2.7 unzip zlib1g-dev lib32gcc1 libc6-dev-i386 subversion flex uglifyjs git-core gcc-multilib p7zip p7zip-full msmtp libssl-dev texinfo libglib2.0-dev xmlto qemu-utils upx libelf-dev autoconf automake libtool autopoint device-tree-compiler g++-multilib antlr3 gperf wget curl swig rsync    
 git config --global user.email 3.1415926535boos@gmail.com
 git config --global user.name Boos4721
 }
@@ -45,30 +42,10 @@ make -j$(nproc) || make -j$(nproc) V=s
 BUILD_END=$(date +"%s")
 }
 
-UPLOAD() {
-mkdir -p ~/UPLOAD && cd ~/src/bin/targets/*/* && mv *.itb *.bin *.ubi sha256sums ~/UPLOAD/      
-curl -fsSL git.io/file-transfer | sh
-./transfer wet -s -p 16 --no-progress ~/UPLOAD 2>&1 | tee cowtransfer.log
-echo "cat cowtransfer.log | grep https"
-}
-
-GITHUB_UPLOAD() {
-cd ~/UPLOAD
-git init
-git remote add origin https://$token@github.com/Boos4721/updater.git
-git checkout -b OpenWrt
-git add .
-git commit -sm "$(date +"%m%d-%H%S")"
-git push -uf origin OpenWrt    
-
-}
-
 SETUP
 CLONE
 MAIN
 BUILD
-UPLOAD
-GITHUB_UPLOAD
 
 DIFF=$(($BUILD_END - $BUILD_START))
 echo "Build completed in $(($DIFF / 60)) minute(s) and $(($DIFF % 60)) seconds"
