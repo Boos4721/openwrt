7fdc40f62b47fe65cc4f71ff0183422e10dd4021
boos4721 <3.1415926535boos@gmail.com>
2021-12-22 13:14:10 +0800

    include: target: add unblockneteasemusic app
    Signed-off-by: boos4721 <3.1415926535boos@gmail.com>

--- include/target.mk
+++ include/target.mk
@@ -50,7 +50,7 @@ DEFAULT_PACKAGES.router:=\
 	luci-newapi block-mount coremark kmod-nf-nathelper kmod-nf-nathelper-extra kmod-ipt-raw kmod-tun \
 	default-settings luci luci-app-ddns luci-app-upnp luci-app-autoreboot \
 	luci-app-filetransfer luci-app-easymesh luci-app-arpbind \
-	luci-app-vlmcsd luci-app-ramfree \
+	luci-app-vlmcsd luci-app-ramfree luci-app-unblockneteasemusic \
 	luci-app-advanced luci-app-argon-config luci-theme-argon luci-app-ttyd \
 	luci-app-turboacc ddns-scripts_aliyun ddns-scripts_dnspod
 
