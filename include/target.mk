3614b49746355f9bba354d6885e899df31dba8be
Ansuel Smith <ansuelsmth@gmail.com>
2022-03-30 16:14:03 +0200

    generic: 5.15: fix aardvak pci pending patch
    Fix aardvak pci controller pending patch. Use generic_handle_domain_irq
    and fix rebase error.
    
    Fixes: 483503603cb2 ("generic: 5.15: rework pending patch")
    Signed-off-by: Ansuel Smith <ansuelsmth@gmail.com>
    Signed-off-by: boos4721 <3.1415926535boos@gmail.com>

--- include/target.mk
+++ include/target.mk
@@ -47,9 +47,9 @@ DEFAULT_PACKAGES.router:=\
 	ppp \
 	ppp-mod-pppoe \
 	UA2F \
-	luci-newapi block-mount coremark kmod-nf-nathelper kmod-nf-nathelper-extra kmod-ipt-raw kmod-tun \
+	luci-newapi block-mount coremark kmod-nf-nathelper kmod-nf-nathelper-extra kmod-ipt-raw kmod-tun kmod-inet-diag \
 	default-settings luci luci-app-ddns luci-app-upnp luci-app-autoreboot \
-	luci-app-filetransfer luci-app-easymesh luci-app-arpbind \
+	luci-app-filetransfer luci-app-eqos luci-app-arpbind \
 	luci-app-vlmcsd luci-app-ramfree luci-app-unblockneteasemusic \
 	luci-app-advanced luci-app-argon-config luci-theme-argon luci-app-ttyd \
 	luci-app-turboacc ddns-scripts_aliyun ddns-scripts_dnspod ddns-scripts_cloudflare.com-v4
