307de6799cbe2e94baaa4c7ec8fa0549bd9525f1
boos4721 <3.1415926535boos@gmail.com>
2022-05-03 09:09:23 +0800

    ipq807x: drop useless devices
    Signed-off-by: boos4721 <3.1415926535boos@gmail.com>

--- target/linux/ipq807x/image/generic.mk
+++ target/linux/ipq807x/image/generic.mk
@@ -30,19 +30,6 @@ define Device/UbiFit-ipq
         IMAGE/nand-sysupgrade.bin := sysupgrade-tar | append-metadata
 endef
 
-define Device/edgecore_eap102
-	$(call Device/FitImage)
-	$(call Device/UbiFit-ipq)
-	DEVICE_VENDOR := Edgecore
-	DEVICE_MODEL := EAP102
-	BLOCKSIZE := 128k
-	PAGESIZE := 2048
-	DEVICE_DTS_CONFIG := config@ac02
-	SOC := ipq8071
-	DEVICE_PACKAGES := ipq-wifi-edgecore_eap102
-endef
-TARGET_DEVICES += edgecore_eap102
-
 define Device/zte_mf269
         $(call Device/FitImage)
         $(call Device/UbiFit)
@@ -55,34 +42,6 @@ define Device/zte_mf269
         DEVICE_PACKAGES := ipq-wifi-zte_mf269 uboot-envtools
 endef
 TARGET_DEVICES += zte_mf269
-
-define Device/edimax_cax1800
-	$(call Device/FitImage)
-	$(call Device/UbiFit)
-	DEVICE_VENDOR := Edimax
-	DEVICE_MODEL := CAX1800
-	BLOCKSIZE := 128k
-	PAGESIZE := 2048
-	DEVICE_DTS_CONFIG := config@ac03
-	SOC := ipq8070
-	DEVICE_PACKAGES := ipq-wifi-edimax_cax1800
-endef
-TARGET_DEVICES += edimax_cax1800
-
-define Device/qnap_301w
-	$(call Device/FitImage)
-	DEVICE_VENDOR := QNAP
-	DEVICE_MODEL := 301w
-	DEVICE_DTS_CONFIG := config@hk01
-	KERNEL_SIZE := 16384k
-	BLOCKSIZE := 512k
-	SOC := ipq8072
-	IMAGES += factory.bin sysupgrade.bin
-	IMAGE/factory.bin := append-rootfs | pad-rootfs | pad-to 64k
-	IMAGE/sysupgrade.bin/squashfs := append-rootfs | pad-to 64k | sysupgrade-tar rootfs=$$$$@ | append-metadata
-	DEVICE_PACKAGES := ipq-wifi-qnap_301w e2fsprogs kmod-fs-ext4 losetup
-endef
-TARGET_DEVICES += qnap_301w
 
 define Device/redmi_ax6
 	$(call Device/xiaomi_ax3600)
@@ -117,3 +76,18 @@ define Device/xiaomi_ax9000
 	DEVICE_PACKAGES := ipq-wifi-xiaomi_ax9000 kmod-ath10k-ct ath10k-firmware-qca9887-ct
 endef
 TARGET_DEVICES += xiaomi_ax9000
+
+define Device/qnap_301w
+	$(call Device/FitImage)
+	DEVICE_VENDOR := QNAP
+	DEVICE_MODEL := 301w
+	DEVICE_DTS_CONFIG := config@hk01
+	KERNEL_SIZE := 16384k
+	BLOCKSIZE := 512k
+	SOC := ipq8072
+	IMAGES += factory.bin sysupgrade.bin
+	IMAGE/factory.bin := append-rootfs | pad-rootfs | pad-to 64k
+	IMAGE/sysupgrade.bin/squashfs := append-rootfs | pad-to 64k | sysupgrade-tar rootfs=$$$$@ | append-metadata
+	DEVICE_PACKAGES := ipq-wifi-qnap_301w e2fsprogs kmod-fs-ext4 losetup
+endef
+TARGET_DEVICES += qnap_301w
