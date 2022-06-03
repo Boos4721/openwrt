2110be622c862e8459b4edef2143466f76bc5903
boos4721 <3.1415926535boos@gmail.com>
2022-06-03 23:43:01 +0800

    target: build dig net tools by default
    Signed-off-by: boos4721 <3.1415926535boos@gmail.com>

--- include/target.mk
+++ include/target.mk
@@ -25,6 +25,7 @@ DEFAULT_PACKAGES:=\
 	uclient-fetch \
 	zsh \
 	git \
+	bind-dig \
 	urandom-seed
 
 ifneq ($(CONFIG_SELINUX),)
@@ -51,7 +52,7 @@ DEFAULT_PACKAGES.router:=\
 	luci-newapi block-mount coremark kmod-nf-nathelper kmod-nf-nathelper-extra kmod-ipt-raw kmod-tun kmod-inet-diag iptables-mod-tproxy iptables-mod-extra \
 	default-settings luci luci-app-ddns luci-app-upnp luci-app-autoreboot \
 	luci-app-filetransfer luci-app-eqos luci-app-arpbind \
-	luci-app-vlmcsd luci-app-ramfree luci-app-unblockneteasemusic \
+	luci-app-ramfree luci-app-unblockneteasemusic \
 	luci-app-advanced luci-app-argon-config luci-theme-argon luci-app-ttyd \
 	luci-app-turboacc ddns-scripts_aliyun ddns-scripts_dnspod ddns-scripts_cloudflare.com-v4
 
