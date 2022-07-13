6a626f7304bb84bcf707fb5b5f83ff3942843cfd
Boos4721 <3.1415926535boos@gmail.com>
2022-07-13 11:24:19 +0800

    rockchip: drop kernel 5.4 and add Radxa 4 series
    Signed-off-by: Boos4721 <3.1415926535boos@gmail.com>

--- target/linux/rockchip/image/armv8.mk
+++ target/linux/rockchip/image/armv8.mk
@@ -3,7 +3,7 @@
 # Copyright (C) 2020 Tobias Maedel
 
 define Device/friendlyarm_nanopi-r2c
-  DEVICE_VENDOR := FriendlyARM
+  DEVICE_VENDOR := FriendlyElec
   DEVICE_MODEL := NanoPi R2C
   SOC := rk3328
   UBOOT_DEVICE_NAME := nanopi-r2c-rk3328
@@ -13,7 +13,7 @@ endef
 TARGET_DEVICES += friendlyarm_nanopi-r2c
 
 define Device/friendlyarm_nanopi-r2s
-  DEVICE_VENDOR := FriendlyARM
+  DEVICE_VENDOR := FriendlyElec
   DEVICE_MODEL := NanoPi R2S
   SOC := rk3328
   UBOOT_DEVICE_NAME := nanopi-r2s-rk3328
@@ -22,17 +22,8 @@ define Device/friendlyarm_nanopi-r2s
 endef
 TARGET_DEVICES += friendlyarm_nanopi-r2s
 
-define Device/friendlyarm_nanopi-r5s
-  DEVICE_VENDOR := FriendlyARM
-  DEVICE_MODEL := NanoPi R5S
-  SOC := rk3328
-  DEVICE_PACKAGES := kmod-r8125 luci-app-emmc-tools
-  DEVICE_DTS = rockchip/rk3328-nanopi-r2s
-endef
-TARGET_DEVICES += friendlyarm_nanopi-r5s
-
 define Device/friendlyarm_nanopi-r4s
-  DEVICE_VENDOR := FriendlyARM
+  DEVICE_VENDOR := FriendlyElec
   DEVICE_MODEL := NanoPi R4S
   SOC := rk3399
   UBOOT_DEVICE_NAME := nanopi-r4s-rk3399
@@ -42,14 +33,24 @@ endef
 TARGET_DEVICES += friendlyarm_nanopi-r4s
 
 define Device/friendlyarm_nanopi-r4se
-  DEVICE_VENDOR := FriendlyARM
+  DEVICE_VENDOR := FriendlyElec
   DEVICE_MODEL := NanoPi R4SE
   SOC := rk3399
   UBOOT_DEVICE_NAME := nanopi-r4se-rk3399
-  IMAGE/sysupgrade.img.gz := boot-common | boot-script nanopi-r4s | pine64-bin | gzip | append-metadata
-  DEVICE_PACKAGES := kmod-r8168 -urngd
+  IMAGE/sysupgrade.img.gz := boot-common | boot-script | pine64-bin | gzip | append-metadata
+  DEVICE_PACKAGES := kmod-r8169 -urngd
 endef
 TARGET_DEVICES += friendlyarm_nanopi-r4se
+
+define Device/friendlyarm_nanopi-r5s
+  DEVICE_VENDOR := FriendlyElec
+  DEVICE_MODEL := NanoPi R5S
+  SOC := rk3568
+  UBOOT_DEVICE_NAME := nanopi-r5s-rk3568
+  IMAGE/sysupgrade.img.gz := boot-common | boot-script nanopi-r5s | pine64-bin | gzip | append-metadata
+  DEVICE_PACKAGES := kmod-r8125
+endef
+TARGET_DEVICES += friendlyarm_nanopi-r5s
 
 define Device/pine64_rockpro64
   DEVICE_VENDOR := Pine64
@@ -61,16 +62,60 @@ define Device/pine64_rockpro64
 endef
 TARGET_DEVICES += pine64_rockpro64
 
-define Device/radxa_rock-pi-4
+define Device/radxa_rock-pi-4a
   DEVICE_VENDOR := Radxa
-  DEVICE_MODEL := ROCK Pi 4
+  DEVICE_MODEL := ROCK Pi 4A
   SOC := rk3399
-  SUPPORTED_DEVICES := radxa,rockpi4
+  SUPPORTED_DEVICES := radxa,rockpi4a
   UBOOT_DEVICE_NAME := rock-pi-4-rk3399
   IMAGE/sysupgrade.img.gz := boot-common | boot-script | pine64-img | gzip | append-metadata
   DEVICE_PACKAGES := -urngd
 endef
-TARGET_DEVICES += radxa_rock-pi-4
+TARGET_DEVICES += radxa_rock-pi-4a
+
+define Device/radxa_rock-pi-4a-plus
+  DEVICE_VENDOR := Radxa
+  DEVICE_MODEL := ROCK Pi 4A Plus
+  SOC := rk3399
+  SUPPORTED_DEVICES := radxa,rockpi4a-plus
+  UBOOT_DEVICE_NAME := rock-pi-4-rk3399
+  IMAGE/sysupgrade.img.gz := boot-common | boot-script | pine64-img | gzip | append-metadata
+  DEVICE_PACKAGES := -urngd
+endef
+TARGET_DEVICES += radxa_rock-pi-4a-plus
+
+define Device/radxa_rock-pi-4b
+  DEVICE_VENDOR := Radxa
+  DEVICE_MODEL := ROCK Pi 4B
+  SOC := rk3399
+  SUPPORTED_DEVICES := radxa,rockpi4b
+  UBOOT_DEVICE_NAME := rock-pi-4-rk3399
+  IMAGE/sysupgrade.img.gz := boot-common | boot-script | pine64-img | gzip | append-metadata
+  DEVICE_PACKAGES := -urngd
+endef
+TARGET_DEVICES += radxa_rock-pi-4b
+
+define Device/radxa_rock-pi-4b-plus
+  DEVICE_VENDOR := Radxa
+  DEVICE_MODEL := ROCK Pi 4B Plus
+  SOC := rk3399
+  SUPPORTED_DEVICES := radxa,rockpi4b-plus
+  UBOOT_DEVICE_NAME := rock-pi-4-rk3399
+  IMAGE/sysupgrade.img.gz := boot-common | boot-script | pine64-img | gzip | append-metadata
+  DEVICE_PACKAGES := -urngd
+endef
+TARGET_DEVICES += radxa_rock-pi-4b-plus
+
+define Device/radxa_rock-pi-4c
+  DEVICE_VENDOR := Radxa
+  DEVICE_MODEL := ROCK Pi 4C
+  SOC := rk3399
+  SUPPORTED_DEVICES := radxa,rockpi4c
+  UBOOT_DEVICE_NAME := rock-pi-4-rk3399
+  IMAGE/sysupgrade.img.gz := boot-common | boot-script | pine64-img | gzip | append-metadata
+  DEVICE_PACKAGES := -urngd
+endef
+TARGET_DEVICES += radxa_rock-pi-4c
 
 define Device/xunlong_orangepi-r1-plus
   DEVICE_VENDOR := Xunlong
